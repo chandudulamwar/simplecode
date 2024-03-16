1b141285c1e1b26d017017c5c9c99c7f5bd77206
chandudulamwar <139040442+chandudulamwar@users.noreply.github.com>
2024-03-16 13:16:27 +0530

    update project

---
+++ src/test/java/Basic/OrganizationsTest.java
@@ -0,0 +1,14 @@
+package Basic;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class OrganizationsTest {
+	@Test
+	public void CreateOrganizationsTest() {
+	WebDriver driver = new ChromeDriver();
+	driver.get("http://localhost:8888/");
+	}
+
+}
